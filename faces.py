61b7584586c3b443e2171be602a335d4c7653d1f
Glen <39155036+thatguuyG@users.noreply.github.com>
2020-12-10 22:02:04 +0300

    testing camera

--- faces.py
+++ faces.py
@@ -1,19 +1,22 @@
 import numpy as np
 import cv2
 
-face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
-
 cap = cv2.VideoCapture(0)
+cap.set(3,640)#set width
+cap.set(4,480)#set height
 
 while(True):
-    #capture frame by frame
     ret, frame = cap.read()
+    frame = cv2.flip(frame, 1) #flip the cam, it straightens it for mine
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    #display the resulting frame
-    cv2.imshow('fame', frame)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
+    cv2.imshow('frame',frame)
+    #gray image
+    cv2.imshow('gray', gray)
+
+    k = cv2.waitKey(30) & 0xff
+    if k == 27 : #press escape to quit
         break
 
-#release capture
 cap.release()
 cv2.destroyAllWindows()
